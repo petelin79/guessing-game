62590331d19d3494b7339c00d458263f29c2b1f6
Dmitry Petelin <petelin.dmitry@gmail.com>
2022-08-10 14:28:18 +0300

    feat: add functionality to resolve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.avg = Math.ceil((this.max+this.min)/2)
+        return this.avg
     }
 
     lower() {
-
+        this.max = this.avg
+        
     }
 
     greater() {
+        this.min = this.avg
 
     }
 }
 
 module.exports = GuessingGame;
+
+
